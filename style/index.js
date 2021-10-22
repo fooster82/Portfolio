183a941640cc668a38aeaffbb407183f14056334
Robert Bristow <robertbristow3@gmail.com>
2021-10-22 12:46:04 +0100

    add styling to about and projects page. Make the navbar shrink when scrolling past a certain threshold. BEginnings of the code to make the intro text 'type' in

--- style/index.js
+++ style/index.js
@@ -1,25 +1,43 @@
 // Nav bar changing size
-const navBar = document.querySelector('nav')
-const navButton = document.getElementsByClassName('nav-button')
+const navButton = document.getElementsByClassName('nav-button');
 
 window.onscroll = () => changeNavSize();
 
 function changeNavSize() {
     if(document.documentElement.scrollTop > 80) {
-        navBar.style.height = '10px';
-        // navButton.style.height = '';
-        navBar.style.transition = 'height 0.2s'
+        for(i=0; i < navButton.length; i++) {
+            navButton[i].style.height = '5px';
+            navButton[i].style.fontSize = '20px';
+        };
     } else {
-        navBar.style.height = '100px';
-        // navButton.style.height = '';
-        navBar.style.transition = 'height 0.2s';
+        for(i=0; i < navButton.length; i++) {
+            navButton[i].style.height = '73px';
+            navButton[i].style.fontSize = '30px';
+        };
     }
 }
+
+
+// Type in the index.html main text
+const indexText = document.getElementById('index-text')
+
+const introText = "Hi! My name is Rob Bristow and I'm currently training as a full stack developer with Futureproof..."
+
+window.onload = () => {
+    for(i=0; i < introText.length; i++) {
+        setTimeout(loadLetter(introText[i]), 300 + 300*i)
+    }
+}
+
+function loadLetter(a) {
+    indexText.textContent += a;
+}
+
 
 // Blinking cursor that shows on the main html page
 const blinker = document.getElementById('blinker')
 
-window.onload = () => setInterval(blinkingCursor, 520)
+// window.onload = () => setInterval(() => blinkingCursor(), 520);
 
 function blinkingCursor() {
     if(!!blinker.textContent) { 
