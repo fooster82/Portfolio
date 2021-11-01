bea9091dcf627214e6db80bf47c6b1485edc8d9a
Robert Bristow <robertbristow3@gmail.com>
2021-11-01 08:18:35 +0000

    remove JS for typing in home text so it loads in normally for now

--- style/index.js
+++ style/index.js
@@ -18,34 +18,34 @@ function changeNavSize() {
 }
 
 
-// Type in the index.html main text
-const indexText = document.getElementById('index-text')
-const introText = "Hi! My name is Rob Bristow and I'm currently training as a full stack developer with Futureproof..."
+// // Type in the index.html main text
+// const indexText = document.getElementById('index-text')
+// const introText = "Hi! My name is Rob Bristow and I'm currently training as a full stack developer with Futureproof..."
 
-let time = 0;
-const arrayText = Array.from(introText);
-// Doesn't like the await on line 30.
-async function loadText() {
-    arrayText.forEach( a => {
-        await delay(time);
-        loadLetter(a);
-        time += 300;
-    })
-};
+// let time = 0;
+// const arrayText = Array.from(introText);
+// // Doesn't like the await on line 30.
+// async function loadText() {
+//     arrayText.forEach( a => {
+//         await delay(time);
+//         loadLetter(a);
+//         time += 300;
+//     })
+// };
 
-delay = ms => {return new Promise(res => setTimeout(res, ms))} 
+// delay = ms => {return new Promise(res => setTimeout(res, ms))} 
 
-loadText()
+// loadText()
 
-function loadLetter(a) {
-    indexText.textContent += a;
-}
+// function loadLetter(a) {
+//     indexText.textContent += a;
+// }
 
 
 // Blinking cursor that shows on the main html page
 const blinker = document.getElementById('blinker')
 
-// window.onload = () => setInterval(() => blinkingCursor(), 520);
+window.onload = () => setInterval(() => blinkingCursor(), 520);
 
 function blinkingCursor() {
     if(!!blinker.textContent) { 
